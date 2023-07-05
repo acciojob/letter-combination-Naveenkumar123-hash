dfd38584b6647e5d33059ba7efe6a2997284ad02
Naveenkumar123-hash <120244421+Naveenkumar123-hash@users.noreply.github.com>
2023-07-05 17:36:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
 function letterCombinations(input_digit) {
-  //Complete the function
-}
+  //Complete the functionif (input_digit.length === 0) {
+    return [];
+  }
+
+  const digitToLetters = {
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz',
+  };
+
+  const combinations = [''];
+
+  for (const digit of input_digit) {
+    const letters = digitToLetters[digit];
+    const newCombinations = [];
+
+    for (const combination of combinations) {
+      for (const letter of letters) {
+        newCombinations.push(combination + letter);
+      }
+    }
+
+    combinations.splice(0);
+    combinations.push(...newCombinations);
+  }
+
+  return combinations.sort();
 
 module.exports = letterCombinations;
